0bc1b0576144f2b2973c802ec1ad4b5f3a184e8f
Richard <richard.esclima@gmail.com>
2022-06-10 20:45:11 -0300

    second commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static("public"))
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // importando as rotas
 const userRoutes = require('./routers/users');
